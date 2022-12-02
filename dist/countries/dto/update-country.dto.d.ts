0e487756a8fb634da353de0c644b2830e6084eea
Kravmaga01 <edwardsalex8034@gmail.com>
2022-12-01 19:44:26 -0500

    remove
    se removio el node_mudeles

--- dist/countries/dto/update-country.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateCountryDto } from './create-country.dto';
-declare const UpdateCountryDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateCountryDto>>;
-export declare class UpdateCountryDto extends UpdateCountryDto_base {
-}
-export {};
--- dist/seed/data/countries.seed.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const COUNTRY_SEED: {
-    name: string;
-    group: string;
-    flag: string;
-    favoritePoint: number;
-    gamesPlayed: number;
-    gamesWon: number;
-    lostMatches: number;
-}[];
