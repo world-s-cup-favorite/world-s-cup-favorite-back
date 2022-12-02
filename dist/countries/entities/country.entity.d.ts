c1387a31add44b7107717acaf18dfb042a9f88d1
Kravmaga01 <edwardsalex8034@gmail.com>
2022-12-01 19:15:37 -0500

    add

--- dist/countries/entities/country.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class NationalFootballTeam {
-    id: string;
-    name: string;
-    flag: string;
-    favoritePoint: number;
-    gamesPlayed: number;
-    gamesWon: number;
-    lostMatches: number;
-}
--- dist/seed/data/users.seed.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const USER_SEED: {
-    name: string;
-    email: string;
-    PassWord: string;
-}[];
