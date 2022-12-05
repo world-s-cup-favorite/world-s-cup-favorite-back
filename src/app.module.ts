a2069b617f01d27a359e4e759ac6ee357b749842
Kravmaga01 <edwardsalex8034@gmail.com>
2022-12-05 18:06:48 -0500

    [add:] se agrego la conecxion
    posgres nube

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,28 +5,22 @@ import { SeedModule } from "./seed/seed.module";
 
 import { CommonModule } from "./common/common.module";
 import { ConfigModule } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
+
 import { GroupsModule } from "./groups/groups.module";
+import { TypeOrmConfigModule } from "./config/typeorm-config.module";
 
 @Module({
   imports: [
-    CommonModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env",
+    }),
+    TypeOrmConfigModule,
     UsersModule,
     FootballTeamModule,
     SeedModule,
     CommonModule,
     GroupsModule,
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
   ],
   providers: [],
 })
